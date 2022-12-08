5fcb7f5bdd01957dc728c03269bff2f099f6fe9f
defontev <veronique.defonte@csgroup.eu>
2022-12-08 15:10:10 +0100

    feat: ARNN plugin : inference and retrain using AI-PILL.

---
+++ pandora_plugin_arnn/model/building_segmentation_model.py
@@ -0,0 +1,330 @@
+#!/usr/bin/env python
+# coding: utf8
+#
+# Copyright (C) 2022 CNES.
+#
+# This file is part of pandora_plugin_arnn
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+This module contains building segmentation neural network.
+"""
+
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+
+
+# pylint: disable=no-member
+class BasicBlock(nn.Module):
+    """Block used in ResNet18 and 34"""
+
+    def __init__(
+        self,
+        in_planes,
+        out_planes,
+        kernel_size,
+        stride=1,
+        padding=0,
+        groups=1,
+        bias=False,
+        conv="classic",
+    ):
+        super(BasicBlock, self).__init__()
+        if conv == "classic":
+            self.conv1 = nn.Conv2d(
+                in_planes,
+                out_planes,
+                kernel_size,
+                stride,
+                padding,
+                groups=groups,
+                bias=bias,
+            )
+            self.bn1 = nn.BatchNorm2d(out_planes)
+            self.relu = nn.ReLU(inplace=True)
+            self.conv2 = nn.Conv2d(
+                out_planes,
+                out_planes,
+                kernel_size,
+                1,
+                padding,
+                groups=groups,
+                bias=bias,
+            )
+            self.bn2 = nn.BatchNorm2d(out_planes)
+
+        self.downsample = None
+        if stride > 1 or in_planes == 12:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_planes),
+            )
+        self.conv_type = conv
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        if self.conv_type == "classic":
+            out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        if self.conv_type == "classic":
+            out = self.bn2(out)
+        if self.downsample is not None:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Encoder34(nn.Module):
+    """Encoder for LinkNet34 (ie ResNet34)"""
+
+    def __init__(
+        self,
+        layer,
+        in_planes,
+        out_planes,
+        kernel_size,
+        stride=1,
+        padding=0,
+        groups=1,
+        bias=False,
+        conv="classic",
+        drop=False,
+    ):
+        super(Encoder34, self).__init__()
+        self.layer = layer
+        if self.layer == 1 or self.layer == 4:
+            self.block1 = BasicBlock(in_planes, out_planes, kernel_size, stride, padding, groups, bias, conv)
+            self.block2 = BasicBlock(out_planes, out_planes, kernel_size, 1, padding, groups, bias, conv)
+            self.block3 = BasicBlock(out_planes, out_planes, kernel_size, 1, padding, groups, bias, conv)
+        if self.layer == 2:
+            self.block1 = BasicBlock(in_planes, out_planes, kernel_size, stride, padding, groups, bias, conv)
+            self.block2 = BasicBlock(out_planes, out_planes, kernel_size, 1, padding, groups, bias, conv)
+            self.block3 = BasicBlock(out_planes, out_planes, kernel_size, 1, padding, groups, bias, conv)
+            self.block4 = BasicBlock(out_planes, out_planes, kernel_size, 1, padding, groups, bias, conv)
+        if self.layer == 3:
+            self.block1 = BasicBlock(in_planes, out_planes, kernel_size, stride, padding, groups, bias, conv)
+            self.block2 = BasicBlock(out_planes, out_planes, kernel_size, 1, padding, groups, bias, conv)
+            self.block3 = BasicBlock(out_planes, out_planes, kernel_size, 1, padding, groups, bias, conv)
+            self.block4 = BasicBlock(out_planes, out_planes, kernel_size, 1, padding, groups, bias, conv)
+            self.block5 = BasicBlock(out_planes, out_planes, kernel_size, 1, padding, groups, bias, conv)
+            self.block6 = BasicBlock(out_planes, out_planes, kernel_size, 1, padding, groups, bias, conv)
+        self.drop = drop
+
+    def forward(self, x):
+        if self.drop:
+            x = nn.Dropout2d(p=0.05)(x)
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        if self.layer == 2:
+            x = self.block4(x)
+        if self.layer == 3:
+            x = self.block4(x)
+            x = self.block5(x)
+            x = self.block6(x)
+        return x
+
+
+class Decoder(nn.Module):
+    """LinkNet(34) decoder"""
+
+    def __init__(
+        self,
+        in_planes,
+        out_planes,
+        kernel_size,
+        stride=1,
+        padding=0,
+        output_padding=0,
+        groups=1,
+        bias=False,
+        drop=False,
+    ):
+        super(Decoder, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_planes, in_planes // 4, 1, 1, 0, bias=bias),
+            nn.BatchNorm2d(in_planes // 4),
+            nn.ReLU(inplace=True),
+        )
+        self.tp_conv = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_planes // 4,
+                in_planes // 4,
+                kernel_size,
+                stride,
+                padding,
+                output_padding,
+                bias=bias,
+            ),
+            nn.BatchNorm2d(in_planes // 4),
+            nn.ReLU(inplace=True),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_planes // 4, out_planes, 1, 1, 0, bias=bias),
+            nn.BatchNorm2d(out_planes),
+            nn.ReLU(inplace=True),
+        )
+        self.drop = drop
+
+    def forward(self, x):
+        if self.drop:
+            x = nn.Dropout2d(p=0.05)(x)
+        x = self.conv1(x)
+        x = self.tp_conv(x)
+        x = self.conv2(x)
+
+        return x
+
+
+class Building_Segmentation(nn.Module):
+    """
+    Generate model architecture. LinkNet34 with interactivity.
+    """
+
+    def __init__(self):
+        """
+        Model initialization
+        """
+        in_channels = 3
+        n_classes = 2
+
+        super(Building_Segmentation, self).__init__()
+        # assume one channel per class
+        self.conv1 = nn.Conv2d(in_channels + n_classes, 64, 7, 2, 3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(3, 2, 1)
+
+        self.encoder1 = Encoder34(1, 64, 64, 3, 1, 1)
+        self.encoder2 = Encoder34(2, 64, 128, 3, 2, 1)
+        self.encoder3 = Encoder34(3, 128, 256, 3, 2, 1)
+        self.encoder4 = Encoder34(4, 256, 512, 3, 2, 1)
+
+        self.decoder1 = Decoder(64, 64, 3, 1, 1, 0)
+        self.decoder2 = Decoder(128, 64, 3, 2, 1, 1)
+        self.decoder3 = Decoder(256, 128, 3, 2, 1, 1)
+        self.decoder4 = Decoder(512, 256, 3, 2, 1, 1)
+
+        # Classifier
+        self.tp_conv1 = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, 3, 2, 1, 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+        )
+        self.conv2 = nn.Sequential(nn.Conv2d(32, 32, 3, 1, 1), nn.BatchNorm2d(32), nn.ReLU(inplace=True))
+        self.tp_conv2 = nn.ConvTranspose2d(32, n_classes, 2, 2, 0)
+
+        self.optimizer = optim.SGD(params=self.parameters(), lr=5 * 10 ** (-6))
+
+    def forward(self, input_):
+        """
+        Forward method
+
+        :param input_: torch 4D array (nb patch, 5, 1024, 1024)
+        :return: network prediction torch 4D array (nb patch, 2, 1024, 1024)
+        """
+        # Initial block
+        input_ = self.conv1(input_)
+        input_ = self.bn1(input_)
+        input_ = self.relu(input_)
+        input_ = self.maxpool(input_)
+
+        # Encoder blocks
+        encoder_1 = self.encoder1(input_)
+        encoder_2 = self.encoder2(encoder_1)
+        encoder_3 = self.encoder3(encoder_2)
+        encoder_4 = self.encoder4(encoder_3)
+
+        # Decoder blocks
+
+        decoder_4 = encoder_3 + self.decoder4(encoder_4)
+        decoder_3 = encoder_2 + self.decoder3(decoder_4)
+        decoder_2 = encoder_1 + self.decoder2(decoder_3)
+        decoder_1 = input_ + self.decoder1(decoder_2)
+
+        # Classifier
+        output_ = self.tp_conv1(decoder_1)
+        output_ = self.conv2(output_)
+        output_ = self.tp_conv2(output_)
+
+        return output_
+
+    def get_classes(self):
+        """Return network's labels
+
+        :return: network's labels
+        :rtype: list(dict)
+        """
+        classes = [
+            {"name": "Not building", "value": 0, "color": "#f5fff4"},
+            {"name": "Building", "value": 1, "color": "#40baac"},
+        ]
+        return classes
+
+    def predict(self, data):
+        """Make prediction
+
+        :param data: Patches
+        :type data: Tensor(number_of_patches, band, row, col)
+        :return: Argmax prediction
+        :rtype: Tensor(number_of_patches, network output, row, col)
+        """
+        prediction = self.forward(data)
+
+        return prediction
+
+    def transform_patches(self, patches):
+        """Apply transformations to patches
+
+        :param patches: list of patches
+        :type patches: np.array(number_of_patches, band, row, col)
+        :return: Tensor of transformed patches
+        :rtype: Tensor(number_of_patches, band, row, col)
+        """
+        patches_expand = np.zeros((patches.shape[0], 5, patches.shape[2], patches.shape[3]), dtype=patches.dtype)
+
+        patches_expand[:, 0, :, :] = (patches[:, 0, :, :] - patches[:, 0, :, :].mean()) / patches[:, 0, :, :].std()
+        patches_expand[:, 1, :, :] = (patches[:, 1, :, :] - patches[:, 1, :, :].mean()) / patches[:, 1, :, :].std()
+        patches_expand[:, 2, :, :] = (patches[:, 2, :, :] - patches[:, 2, :, :].mean()) / patches[:, 2, :, :].std()
+
+        patches_expand = torch.from_numpy(patches_expand)
+
+        return patches_expand
+
+    def get_patch_size(self):
+        """Return integer patch size
+
+        :return: patch size
+        :rtype: int
+        """
+        return 1024
+
+    def backward(self, loss):
+        """Make backward using custom loss
+
+        :param loss: loss to backward
+        """
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
--- pandora_plugin_arnn/pandora_plugin_arnn.py
+++ pandora_plugin_arnn/pandora_plugin_arnn.py
@@ -19,6 +19,266 @@
 #
 
 """
-Main module.
-DOC: describe the module aim !
+This module contains all functions to compute and refine a semantic segmentation map for Pandora
 """
+
+import xarray as xr
+import torch
+from json_checker import Checker, Or, And
+from typing import Dict, Union
+import numpy as np
+from scipy import ndimage
+from importlib_resources import files
+
+from pandora.semantic_segmentation import semantic_segmentation
+from pandora_plugin_arnn.model.building_segmentation_model import Building_Segmentation
+from ai_pil.ai.prediction import prediction
+from ai_pil.ai.retrain import retrain
+from pandora.disparity import AbstractDisparity
+from pandora import constants as cst
+
+
+@semantic_segmentation.AbstractSemanticSegmentation.register_subclass("ARNN")
+class ARNN(semantic_segmentation.AbstractSemanticSegmentation):
+    """
+    ARNN class is a Pandora plugin that perform building segmentation using a pre-trained network on VHR images.
+    It also allows to refine the segmentation map
+    """
+
+    # Default configuration, do not change these values
+    _REFINEMENT_ = False
+
+    def __init__(self, **cfg: Dict[str, Union[str, int]]) -> None:
+        """
+        :param cfg: optional configuration,  {'window_size': value, 'subpix': value}
+        :type cfg: dict
+        :return: None
+        """
+        self.cfg = self.check_conf(**cfg)
+        self._refinement = self.cfg["refinement"]
+        self._RGBNIR_bands = self.cfg["RGBNIR_bands"]
+
+    def check_conf(self, **cfg: Dict[str, Union[str, int]]) -> Dict[str, Union[str, int]]:
+        """
+        Add default values to the dictionary if there are missing elements and check if the dictionary is correct
+
+        :param cfg: matching cost configuration
+        :type cfg: dict
+        :return cfg: matching cost configuration updated
+        :rtype: dict
+        """
+        # Give the default value if the required element is not in the conf
+        if "refinement" not in cfg:
+            cfg["refinement"] = self._REFINEMENT_  # type: ignore
+
+        schema = {
+            "semantic_segmentation_method": And(str, lambda x: x == "ARNN"),
+            "RGBNIR_bands": {"R": str, "G": str, "B": str, "NIR": Or(str, lambda input: input is None)},
+            "refinement": bool,
+        }
+
+        # TODO : check if bands exist
+
+        checker = Checker(schema)
+        checker.validate(cfg)
+        return cfg  # type: ignore
+
+    def desc(self):
+        """
+        Describes the method
+        """
+        print("Building semantic segmentation")
+
+    def compute_semantic_segmentation(self, cv: xr.Dataset, img_left: xr.Dataset, img_right: xr.Dataset) -> xr.Dataset:
+        """
+        Compute building semantic segmentation
+
+        :param cv: the cost volume, with the data variables:
+
+                - cost_volume 3D xarray.DataArray (row, col, disp)
+                - confidence_measure (optional): 3D xarray.DataArray (row, col, indicator)
+        :type cv: xarray.Dataset
+        :param img_left: left Dataset image containing :
+
+                - im : 2D (row, col) xarray.DataArray
+                - msk (optional): 2D (row, col) xarray.DataArray
+        :type img_left: xarray
+        :param img_right: right Dataset image containing :
+
+                - im : 2D (row, col) xarray.DataArray
+                - msk (optional): 2D (row, col) xarray.DataArray
+        :type img_right: xarray
+        :return: The building segmentation in the left image dataset with the data variables:
+
+                - im : 2D (row, col) xarray.DataArray
+                - msk (optional): 2D (row, col) xarray.DataArray
+                - initial : 2D (row, col) xarray.DataArray building segmentation
+        :rtype: xarray.Dataset
+        """
+        # Load pretrained building segmentation model
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model = Building_Segmentation()
+        model = model.eval()
+        pretrained_path = files("pandora_plugin_arnn.model").joinpath("pretrained_building_segmentation_model.pt")
+        model.load_state_dict(torch.load(pretrained_path))
+        model = model.to(device)
+
+        row, begin_row = (len(img_left.coords["row"]), 0)
+        col, begin_col = (len(img_left.coords["col"]), 0)
+
+        # Padd image to the size of the model if needed
+        if row < 1024:
+            row, begin_row = (1024, 1024 - row)
+        if col < 1024:
+            col, begin_col = (1024, 1024 - col)
+
+        # Extract RGB img
+        rgb_img = np.zeros((3, row, col), dtype=np.float32)
+
+        for indx, band in enumerate(["R", "G", "B"]):
+            band_index = img_left.attrs["band_list"].index(self._RGBNIR_bands[band])  # type: ignore
+            rgb_img[indx, begin_row:, begin_col:] = np.copy(img_left["im"].data[:, :, band_index])
+
+        model_dataset = xr.Dataset(
+            {"im": (["band", "row", "col"], rgb_img.astype(np.float32))},
+            coords={
+                "band": np.arange(rgb_img.shape[0]),
+                "row": np.arange(rgb_img.shape[1]),
+                "col": np.arange(rgb_img.shape[2]),
+            },
+        )
+
+        # Initial prediction
+        model_dataset = prediction(model_dataset, model, device)
+
+        # Make refinement if needed
+        if self._refinement:
+            # Computes annotation map
+            annotation = self.compute_annotations(cv, img_left, img_right, model_dataset["initial_prediction"].data)
+
+            model_dataset["annotation"] = xr.DataArray(
+                data=annotation, coords=[model_dataset.coords["row"], model_dataset.coords["col"]], dims=["row", "col"]
+            )
+
+            # Retrain the network with the annotation map and make a new prediction
+            model_dataset = retrain(model, model_dataset, device, retrain_epoch=1, ignore_index=-1)
+
+        # Recrop the segmentation to original image size
+        img_left["internal"] = xr.DataArray(
+            data=model_dataset["initial_prediction"].data[begin_row:, begin_col:],
+            coords=[img_left.coords["row"], img_left.coords["col"]],
+            dims=["row", "col"],
+        )
+
+        return img_left
+
+    def compute_annotations(
+        self, cv: xr.Dataset, img_left: xr.Dataset, img_right: xr.Dataset, initial_prediction: np.ndarray
+    ) -> np.ndarray:
+        """
+        Create an annotation card to retrain the model
+
+        :param cv: the cost volume, with the data variables:
+
+                - cost_volume 3D xarray.DataArray (row, col, disp)
+                - confidence_measure (optional): 3D xarray.DataArray (row, col, indicator)
+        :type cv: xarray.Dataset
+        :param img_left: left Dataset image containing :
+
+                - im : 2D (row, col) xarray.DataArray
+                - msk (optional): 2D (row, col) xarray.DataArray
+        :type img_left: xarray
+        :param img_right: right Dataset image containing :
+
+                - im : 2D (row, col) xarray.DataArray
+                - msk (optional): 2D (row, col) xarray.DataArray
+        :type img_right: xarray
+        :param initial_prediction: First prediction of the model
+        :type initial_prediction: 2D (row, col) np.array
+        :return: The building segmentation in the left image dataset with the data variables:
+
+                - im : 2D (row, col) xarray.DataArray
+                - msk (optional): 2D (row, col) xarray.DataArray
+                - initial : 2D (row, col) xarray.DataArray building segmentation
+        :rtype: xarray.Dataset
+        """
+        # Apply WTA on cost volume
+        wta = AbstractDisparity(**{"disparity_method": "wta", "invalid_disparity": -9999})
+        disp = wta.to_disp(cv, img_left)
+        wta.validity_mask(disp, img_left, img_right, cv)
+
+        # Find the threshold on the disparity map to dissociate the ground from the overground
+        ground_threshold = self.ground_extraction(disp)
+
+        # If Pandora has not calculated confidence, we consider that all points are confident
+        ambiguity = np.ones((len(img_left.coords["row"]), len(img_left.coords["col"])), dtype=np.float32)
+        if "confidence_measure" in cv:
+            if "ambiguity_confidence" in cv.coords["indicator"]:
+                ambiguity = cv["confidence_measure"].loc[:, :, "ambiguity_confidence"].data
+
+        # Create annotation map
+        annotation = np.full(initial_prediction.shape, -1, dtype=np.float32)
+
+        # Extract overground pixels
+        annotation[np.where((disp["disparity_map"].data <= ground_threshold) & (ambiguity >= 0.90))] = 1
+        # Extract non-buildings pixels
+        annotation[
+            np.where(
+                (disp["disparity_map"].data > (ground_threshold + 1)) & (ambiguity >= 0.90) & (initial_prediction == 1)
+            )
+        ] = 0
+        # Remove invalid pixels
+        annotation[np.where(disp["disparity_map"].data == -9999)] = -1
+        invalids = (
+            cst.PANDORA_MSK_PIXEL_LEFT_NODATA_OR_BORDER
+            + cst.PANDORA_MSK_PIXEL_RIGHT_NODATA_OR_DISPARITY_RANGE_MISSING
+            + cst.PANDORA_MSK_PIXEL_RIGHT_INCOMPLETE_DISPARITY_RANGE
+            + cst.PANDORA_MSK_PIXEL_IN_VALIDITY_MASK_LEFT
+            + cst.PANDORA_MSK_PIXEL_IN_VALIDITY_MASK_RIGHT
+        )
+        annotation[np.where((disp["validity_mask"].data & invalids) != 0)] = -1
+
+        # Find vegetation
+        vegetation_map = self.compute_vegetation_map(cv, img_left)
+
+        # Dilates the vegetation map and remove vegetation pixel in annotation map
+        vegetation_map = ndimage.binary_dilation(vegetation_map, structure=np.ones((10, 10))).astype(
+            vegetation_map.dtype
+        )
+        annotation[vegetation_map == 1] = -1
+
+        return annotation
+
+    def ground_extraction(self, wta: xr.Dataset):
+        """
+        KMeans for ground/over-ground point classification
+
+        :param wta: Disparities
+        :type wta: 2D (row, col) np.array
+        """
+        # TODO :  KMeans for ground/over-ground point classification
+        # Returns null value for now
+        return 0
+
+    def compute_vegetation_map(self, cv: xr.Dataset, img_left: xr.Dataset):
+        """
+        Compute vegetation map
+
+        :param cv: the cost volume, with the data variables:
+
+                - cost_volume 3D xarray.DataArray (row, col, disp)
+                - confidence_measure (optional): 3D xarray.DataArray (row, col, indicator)
+        :type cv: xarray.Dataset
+        :param img_left: left Dataset image containing :
+
+                - im : 2D (row, col) xarray.DataArray
+                - msk (optional): 2D (row, col) xarray.DataArray
+        :type img_left: xarray
+        :return: Vegetation map (0 = not vegetation, 1 = vegetation)
+        :rtype: 2D (row, cool) np.array dtype=bool
+        """
+        # TODO : Compute the vegetation map with a threshold / clustering on the NDVI
+        # vegetation map (0 = not vegetation, 1 = vegetation)
+        vegetation_map = np.zeros((len(img_left.coords["row"]), len(img_left.coords["col"])), dtype=np.bool)  # type: ignore
+
+        return vegetation_map

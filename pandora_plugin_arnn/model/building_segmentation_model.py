fix: Pad image with nan value. Patch normalization using nan operations

pandora_plugin_arnn/model/building_segmentation_model.py
@@ -304,10 +304,17 @@ class Building_Segmentation(nn.Module):
         """
         patches_expand = np.zeros((patches.shape[0], 5, patches.shape[2], patches.shape[3]), dtype=patches.dtype)
 
-        patches_expand[:, 0, :, :] = (patches[:, 0, :, :] - patches[:, 0, :, :].mean()) / patches[:, 0, :, :].std()
-        patches_expand[:, 1, :, :] = (patches[:, 1, :, :] - patches[:, 1, :, :].mean()) / patches[:, 1, :, :].std()
-        patches_expand[:, 2, :, :] = (patches[:, 2, :, :] - patches[:, 2, :, :].mean()) / patches[:, 2, :, :].std()
+        patches_expand[:, 0, :, :] = (patches[:, 0, :, :] - np.nanmean(patches[:, 0, :, :])) / np.nanstd(
+            patches[:, 0, :, :]
+        )
+        patches_expand[:, 1, :, :] = (patches[:, 1, :, :] - np.nanmean(patches[:, 1, :, :])) / np.nanstd(
+            patches[:, 1, :, :]
+        )
+        patches_expand[:, 2, :, :] = (patches[:, 2, :, :] - np.nanmean(patches[:, 2, :, :])) / np.nanstd(
+            patches[:, 2, :, :]
+        )
 
+        patches_expand[np.isnan(patches_expand)] = 0
         patches_expand = torch.from_numpy(patches_expand)
 
         return patches_expand

pandora_plugin_arnn/pandora_plugin_arnn.py
@@ -133,7 +133,7 @@ class ARNN(semantic_segmentation.AbstractSemanticSegmentation):
             col, begin_col = (1024, 1024 - col)
 
         # Extract RGB img
-        rgb_img = np.zeros((3, row, col), dtype=np.float32)
+        rgb_img = np.full((3, row, col), fill_value=np.nan, dtype=np.float32)
 
         for indx, band in enumerate(["R", "G", "B"]):
             band_index = img_left.attrs["band_list"].index(self._RGBNIR_bands[band])  # type: ignore